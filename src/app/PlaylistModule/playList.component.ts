99be20f8a26c5ec456f546f2b5d66c6b42b3ee10
Kavshree <kavyashri.bj@gmail.com>
2021-03-14 18:17:35 -0400

    implemented playlist basic logic

--- src/app/PlaylistModule/playList.component.ts
+++ src/app/PlaylistModule/playList.component.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
 import  { KTuneService } from '../ktune.service';
 import { CardCarouselComponent } from './cardcarousel.component';
+
 
 @Component({
     selector: 'play-list',
@@ -8,10 +9,19 @@ import { CardCarouselComponent } from './cardcarousel.component';
     <div class="col-lg-12">
     <h3> Playlist </h3>
     <hr class="ktunesLine" />
-    <div class="card-columns card-deck">
-    <div class="card mb-3" style="min-width: 18rem;" *ngFor="let play of playListData">
 
-    </div>
+    <div class="card-columns card-deck">
+        <div class="card mb-3" style="min-width: 18rem;" *ngFor="let play of playListData">
+            <div class="card-header">{{play.playListName | uppercase }}</div>
+        <div class="flex-nowrap">
+            <div class="list-group" *ngFor="let p of play.PlayListSongs">
+                <div class="list-group-item">
+                   <p>Song {{p.songname}} {{p.songID}}</p>
+                   <p class="text-muted"> Album: {{ getSongDetailsFromID(p.songID, "album") }} </p>
+                </div>
+            </div>
+        </div>
+        </div>
     </div>
 
 
@@ -45,29 +55,35 @@ import { CardCarouselComponent } from './cardcarousel.component';
         margin: 10px;
         padding: 10px;
         border: 1px solid black;
-    }
+    },
     `]
 })
 
 export class PlayListComponent {
-
-    response; playListData=[];
+    response; playListData=[];allSongs;;
     playListBlueprint={"playListName":"", "PlayListSongs":[]};
     constructor(private _service: KTuneService) {}
     ngOnInit() {
+
+        this._service.getSongs().subscribe(res => {
+            this.allSongs = res;
+        })
+
         this._service.getPlayList().subscribe(res => {
             this.response=res;
             //console.log(res);
 
             for(var i=0;i<this.response.length;i++) {
                 let present = this.isPlaydataContainsPlaylist(this.response[i].playListName);
+                let sName = this.getSongDetailsFromID(this.response[i].songID, "songname");
+                 console.log(sName)
 
                 if(present == -1) {
                  this.playListBlueprint.playListName = this.response[i].playListName;
-                 this.playListBlueprint.PlayListSongs.push({ "songID": [this.response[i].songID]});
+                 this.playListBlueprint.PlayListSongs.push({ "songID": this.response[i].songID, "songname": sName} );
                 this.playListData.push(this.playListBlueprint);
                 } else {
-                    this.playListData[present].PlayListSongs.push({ "songID": [this.response[i].songID]});
+                    this.playListData[present].PlayListSongs.push({ "songID": this.response[i].songID, "songname": sName});
                 }
                 
                 this.playListBlueprint={"playListName":"", "PlayListSongs":[]};
@@ -89,6 +105,17 @@ export class PlayListComponent {
         } 
         return indexVal;
     }
+
+    getSongDetailsFromID(id, field) {
+        let songFromID; 
+        for(var i=0;i<this.allSongs.length; i++) {
+            if(this.allSongs[i].id == id) {
+                songFromID = this.allSongs[i][field]
+            }
+        }
+        return songFromID;
+    }
+
 
 
 
